868ccea43e9a1063663053c482bbb2946473a1fb
Matej Zelenak <121208236+caawww@users.noreply.github.com>
2024-04-14 00:15:56 +0200

    Revert "add base cpp project"
    This reverts commit 9d2e14592a50644c9695ac76e95ce26431e13546.

--- gameProject/Source/gamesa/gamesa.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "gamesa.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gamesa, "gamesa" );
